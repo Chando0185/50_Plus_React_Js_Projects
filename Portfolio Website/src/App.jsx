aae07e14c4cea24345a2fa6f95ba275844a0f537
Mishu Dhar Chando <53133093+Chando0185@users.noreply.github.com>
2024-11-16 12:35:16 +0600

    Add files via upload

---
+++ Portfolio Website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+const App = () => {
+  return (
+    <div className="bg-black text-white">
+      <Header></Header>
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
